795a5029e0a41bf1238bb63276d68af7a2e7e48b
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-07-27 22:09:17 -0300

    authentication

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -21,6 +21,16 @@ const generarJWT = (uid) => {
   });
 };
 
+const comprobarJWT = (token = "") => {
+  try {
+    const { uid } = jwt.verify(token, process.env.JWT_SECRET);
+    return [true, uid];
+  } catch (error) {
+    return [false, null];
+  }
+};
+
 module.exports = {
   generarJWT,
+  comprobarJWT,
 };
--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,9 @@
+const {
+  usuarioConectado,
+  usuarioDesconectado,
+} = require("../controllers/sockets");
+const { comprobarJWT } = require("../helpers/jwt");
+
 class Sockets {
   constructor(io) {
     this.io = io;
@@ -5,7 +11,15 @@ class Sockets {
   }
   socketEvents() {
     // On connection
-    this.io.on("connection", (socket) => {
+    this.io.on("connection", async (socket) => {
+      const [valido, uid] = comprobarJWT(socket.handshake.query["x-token"]);
+      if (!valido) {
+        console.log("Socket no identificado");
+        return socket.disconnect();
+      }
+
+      await usuarioConectado(uid);
+
       //TODO:Validar el JWT
       //Si el token no es valido, desconectar
       //TODO:Saber que usuario esta activo mediante el UID
@@ -14,6 +28,10 @@ class Sockets {
       //TODO:Escuchar cuando el cliente manda un mensaje
       //mensaje-personal
       //TODO:Disconect
+      socket.on("disconnect", async () => {
+        console.log("cliente desconectado", uid);
+        await usuarioDesconectado(uid);
+      });
       //Marcar en la DB que el usuario se desconecto
       //TODO:emitir todos los usuarios conectados
     });
